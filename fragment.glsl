7f79e303aa22b2d987e20668f6722e325f1e3063
Evan Wright <evanpw@gmail.com>
2013-04-08 09:29:25 -0400

    Get rid of ugly hack to overwrite texture alphas.

--- fragment.glsl
+++ fragment.glsl
@@ -10,6 +10,6 @@ out vec4 fragColor;
 void main()
 {
 	//fragColor = mix(vec4(color, 1.0), texture(textureSampler, uv), 0.5);
-	fragColor = vec4(vec3(texture(textureSampler, uv)), 1.0);
+	fragColor = texture(textureSampler, uv);
 	//fragColor = vec4(color, 0.5);
 }
